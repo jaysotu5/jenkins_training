47a14b656fd36eb20d6c01a02e75be9d6c7bff8b
Sotu <Sotu>
2025-02-23 20:18:11 -0600

    first 2  commit

--- demo.py
+++ demo.py
@@ -1,3 +1,2 @@
-hello everyone
-Enjoy Jenkins on Sunday
-this message is from python code used in jenkins build
+hello everyone; Enjoy Jenkins on Sunday; this message is from python code used in jenkins build
+
